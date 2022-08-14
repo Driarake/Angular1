1906b9cfa42ca1ff5af4694a97cb0ca18b542002
Driarake <adriana_drix@yahoo.com.br>
2022-08-14 19:10:40 -0300

    Angular1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular1');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular1';
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = 'Adriana Arake'
+  text = '';
+
+  pessoas = [
+    {
+      nome: "Adriana",
+      sobrenome: "Arake"
+    },
+    {
+      nome: "Joao",
+      sobrenome: "Santos"
+    },
+    {
+      nome: "Ana",
+      sobrenome: "Galvão"
+    }
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log(this.pessoas);
+    let interval = setInterval(() => {
+      this.count++
+      if (this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
+  clicou(nome: string): void {
+    console.log('Clicou em mim', nome)
+  }
 }
